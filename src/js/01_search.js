ef3768e580abe08674fc9736d3da66b19ba30b6e
Patuco <patricia.beco.dg@gmail.com>
2023-02-24 11:37:37 +0100

    Create search logic and connect it with API

--- src/js/01_search.js
+++ src/js/01_search.js
@@ -1,7 +1,95 @@
-// Connect with cocktail API when clicking SEARCH BUTTON + show searched info
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
+/* eslint-disable strict */
+"use strict";
 
-function handleBtnSearch(ev){
-ev.preventDefault();
+// Show by default the list of margaritas before doing any search
+printDefault();
+
+// Function for printing margaritas list
+function printDefault() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      drinksList.innerHTML = "";
+
+      for (const drink of data.drinks) {
+        const liElement = document.createElement("li");
+        liElement.setAttribute("class", "main__drinks-list--item");
+        drinksList.appendChild(liElement);
+
+        const nameElement = document.createElement("h3");
+        const textName = document.createTextNode(drink.strDrink);
+        nameElement.appendChild(textName);
+        liElement.setAttribute("class", "main__drinks-list--item--name");
+        liElement.appendChild(nameElement);
+
+        // Add a placeholder image if the cocktail doesn't contain an image
+        if (drink.strDrinkThumb) {
+          const imgElement = document.createElement("img");
+          liElement.setAttribute("class", "main__drinks-list--item--img");
+          imgElement.setAttribute("src", `${drink.strDrinkThumb}/preview`);
+          liElement.appendChild(imgElement);
+        } else {
+          const imgElement = document.createElement("img");
+          liElement.setAttribute("class", "main__drinks-list--item--img");
+          imgElement.setAttribute(
+            "src",
+            "https://via.placeholder.com/210x295/ffffff/666666/?text=TV"
+          );
+          liElement.appendChild(imgElement);
+        }
+      }
+    });
+}
+
+// Function for printing the result of input value search
+function printInput() {
+  const inputValue = inputSearch.value;
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      drinksList.innerHTML = "";
+
+      for (const drink of data.drinks) {
+        const liElement = document.createElement("li");
+        liElement.setAttribute("class", "main__drinks-list--item");
+        drinksList.appendChild(liElement);
+
+        const nameElement = document.createElement("h3");
+        const textName = document.createTextNode(drink.strDrink);
+        nameElement.appendChild(textName);
+        liElement.setAttribute("class", "main__drinks-list--item--name");
+        liElement.appendChild(nameElement);
+
+        // Add a placeholder image if the cocktail doesn't contain an image
+        if (drink.strDrinkThumb) {
+          const imgElement = document.createElement("img");
+          liElement.setAttribute("class", "main__drinks-list--item--img");
+          imgElement.setAttribute("src", `${drink.strDrinkThumb}/preview`);
+          liElement.appendChild(imgElement);
+        } else {
+          const imgElement = document.createElement("img");
+          liElement.setAttribute("class", "main__drinks-list--item--img");
+          imgElement.setAttribute(
+            "src",
+            "https://via.placeholder.com/210x295/ffffff/666666/?text=TV"
+          );
+          liElement.appendChild(imgElement);
+        }
+      }
+    });
+}
+
+// Listen to click event and show list of cocktails
+function handleBtnSearch(ev) {
+  console.log("funcionaaa");
+  ev.preventDefault();
+  printInput();
 }
 
 btnSearch.addEventListener("click", handleBtnSearch);
