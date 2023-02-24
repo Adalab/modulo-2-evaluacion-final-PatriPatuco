7fc246fbcd41706af1b1ea2be9cd0e803fcf3282
Patuco <patricia.beco.dg@gmail.com>
2023-02-24 16:12:51 +0100

    Change search.js functions structure

--- src/js/00_variables.js
+++ src/js/00_variables.js
@@ -9,6 +9,10 @@ const btnReset = document.querySelector(".js__reset-button");
 
 // Cocktail list section
 const drinksList = document.querySelector(".js__drinks-list");
+const drinkItem = document.querySelector(".js__drink");
 
 // Favorites list section
 const favList = document.querySelector(".js__fav-list");
+
+// Cocktails data
+let cocktailsData = [];
